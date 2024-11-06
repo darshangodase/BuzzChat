25eb1b810d0e6f3f83b71cc7da1ad85e8001d78c
darshangodase <darshangodase10@gmail.com>
2024-11-06 13:44:36 +0530

    index file change insocket folder

--- server/socket/index.js
+++ server/socket/index.js
@@ -11,7 +11,7 @@ const getUserDetailsFromToken= require('../helpers/getUserDetails')
 const server = http.createServer(app)
 const io = new Server(server,{
     cors : {
-        origin : process.env.FRONTEND_URL,
+        origin : 'https://buzzchatapp.netlify.app',
         credentials : true
     }
 })
